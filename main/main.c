96d07a9d645a5dd2cecd5a94cce7087be09b0209
Homin Lee <homin.lee@suapapa.net>
2021-02-20 09:55:17 +0900

    add camera code

--- main/main.c
+++ main/main.c
@@ -16,9 +16,13 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "mqtt_client.h"
+#include "esp_camera.h"
+#include "driver/gpio.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
+
+#define BOARD_ESP32CAM_AITHINKER
 
 /* Values from menuconfig */
 #define WIFI_SSID      CONFIG_ESP_WIFI_SSID
@@ -170,8 +174,114 @@ void init_mqtt(void)
 
 /* ---- */
 
+// ESP32Cam (AiThinker) PIN Map
+#ifdef BOARD_ESP32CAM_AITHINKER
+
+#define BLINK_GPIO 33
+
+#define CAM_PIN_PWDN 32
+#define CAM_PIN_RESET -1	//software reset will be performed
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+
+#endif
+
+static camera_config_t camera_config = {
+	.pin_pwdn = CAM_PIN_PWDN,
+	.pin_reset = CAM_PIN_RESET,
+	.pin_xclk = CAM_PIN_XCLK,
+	.pin_sscb_sda = CAM_PIN_SIOD,
+	.pin_sscb_scl = CAM_PIN_SIOC,
+
+	.pin_d7 = CAM_PIN_D7,
+	.pin_d6 = CAM_PIN_D6,
+	.pin_d5 = CAM_PIN_D5,
+	.pin_d4 = CAM_PIN_D4,
+	.pin_d3 = CAM_PIN_D3,
+	.pin_d2 = CAM_PIN_D2,
+	.pin_d1 = CAM_PIN_D1,
+	.pin_d0 = CAM_PIN_D0,
+	.pin_vsync = CAM_PIN_VSYNC,
+	.pin_href = CAM_PIN_HREF,
+	.pin_pclk = CAM_PIN_PCLK,
+
+	//XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+	.xclk_freq_hz = 20000000,
+	.ledc_timer = LEDC_TIMER_0,
+	.ledc_channel = LEDC_CHANNEL_0,
+
+	.pixel_format = PIXFORMAT_JPEG,	//YUV422,GRAYSCALE,RGB565,JPEG
+	.frame_size = FRAMESIZE_VGA,	//QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+	.jpeg_quality = 12,	//0-63 lower number means higher quality
+	.fb_count = 1		//if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
+static esp_err_t init_camera()
+{
+	//initialize the camera
+	esp_err_t err = esp_camera_init(&camera_config);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Camera Init Failed");
+		return err;
+	}
+
+	return ESP_OK;
+}
+
+/* ---- */
+
+void init_gpio()
+{
+	gpio_pad_select_gpio(BLINK_GPIO);
+	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+/* ---- */
+
+void process_image(size_t w, size_t h, pixformat_t fmt, uint8_t * buf,
+		   size_t buf_len)
+{
+	// TODO: grayscale bmp
+	// TODO: timestamp
+	ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", buf_len);
+	esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC, (const char *)(buf),
+				buf_len, 0, 0);
+}
+
+/* ---- */
+
 void app_main(void)
 {
 	init_wifi();
 	init_mqtt();
+	init_camera();
+	init_gpio();
+
+	while (1) {
+		ESP_LOGI(TAG, "Taking picture...");
+		gpio_set_level(BLINK_GPIO, 1);
+		camera_fb_t *fb = esp_camera_fb_get();
+
+		process_image(fb->width, fb->height, fb->format, fb->buf,
+			      fb->len);
+
+		esp_camera_fb_return(fb);
+		gpio_set_level(BLINK_GPIO, 0);
+
+		vTaskDelay(10000 / portTICK_RATE_MS);
+	}
 }
